416879756c7736828b3f3e2524abf0bd6fdcbb39
SajibSarker113 <66987905+SajibSarker113@users.noreply.github.com>
2020-07-17 19:21:22 +0600

    1,2,4 complete

---
+++ assignment.js
@@ -0,0 +1,25 @@
+// function feetToMile(feet){
+//     var mile = feet/5280;
+//     return mile;
+// }
+
+
+// function woodCalculator(chair, table, khat){
+//     var chairCubicFeet = chair * 1;
+//     var tableCubicFeet = table * 3;
+//     var khatCubicFeet =khat * 5;
+//     var totalCubicFeet = chairCubicFeet + tableCubicFeet + khatCubicFeet;
+//     return totalCubicFeet;
+// }
+
+
+function tinyFriend(name) {
+    var smallerName = name[0];
+    for (var i = 0; i < name.length; i++) {
+        var currentName = name[i];
+        if(currentName.length < smallerName.length) {
+            smallerName = currentName;
+        }
+    }
+    return smallerName;
+}
